5db530ea45b96fece9b6864e095370c22ba73b32
deuro <hero14yki1986@gmail.com>
2021-02-06 17:39:40 +0000

    Finish add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
   
   def index
     @users = User.paginate(page: params[:page])
   end
   
   def show
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
   
   def new
@@ -79,7 +81,7 @@ class UsersController < ApplicationController
    end
    
    def correct_user
-     redirect_to(root_url) unless @user == current_user
+     redirect_to(root_url) unless current_user?(@user)
    end
    
    def admin_user
